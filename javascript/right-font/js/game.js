846f0933e42c1de8684d09a1f72338b0ffe02370
BleexL <sagrado.axel@gmail.com>
2023-10-18 14:29:03 +0200

    First game ok + bug restart + rnadom  word

--- javascript/right-font/js/game.js
+++ javascript/right-font/js/game.js
@@ -13,10 +13,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
     
     let score = 0;
     let record = 0;
-    let homeSection = true;
+    // let homeSection = true;
     let gameOn = true;
-    let endgame = false;
+    // let endgame = true;
     
+    let gameState = {
+        home : "home",
+        starting : "starting",
+        game : "game",
+        endGame : "endGame",
+    };
+    let currentGameState = gameState.home;
+
     let homeContainer = document.getElementsByClassName("home-container")[0];
     let gameContainer = document.getElementsByClassName("game-container")[0];
     let endgamePopUp = document.getElementsByClassName("pop-up-container")[0];
@@ -25,12 +33,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
     let model = document.getElementById("model");
     let demoHamburger = document.getElementById("demoHamburger");
 
-    let fontTarget = demoHamburger
+    let fontTarget = demoHamburger;
 
     const verifContainer = document.getElementsByClassName("mouse-controler-container")[0];
     const startButton = document.getElementById("start");
+    const restartButton = document.getElementById("restart");
+    const homePageButton = document.getElementById("home-page");
+    const wordList = ["Hamburger", "Bonjour", "Soleil", "Chocolat", "Danse", "Amour", "Écouter", "Musique", "Bonheur", "Voyage", "Papillon"]
 
-    let countdown = 60;
+    let countdown = 5;
     let timeRemaining = countdown;
     let gaugeRemaining = 100;
     let timerGauge = document.querySelector('.timer-gauge');
@@ -42,15 +53,17 @@ document.addEventListener("DOMContentLoaded", (event) => {
     
     // Fonction compte à rebours
     function updateCountdown (){
-        if (timeRemaining > 0){
-            timeRemaining = timeRemaining - 0.1;
-            // document.getElementById("countdown").textContent=timeRemaining;
+        if (timeRemaining <= 0){
+            gameOn = false;
+            currentGameState = gameState.endGame;
 
-            gaugeRemaining = timeRemaining*100/60;
-            timerGauge.style.width = gaugeRemaining + '%';
+            handleGameState();
         }
         else {
-            gameOn = false;
+            activeTime = setTimeout(updateCountdown, 100)
+            timeRemaining = timeRemaining - 0.1;
+            gaugeRemaining = timeRemaining*100/60;
+            timerGauge.style.width = gaugeRemaining + '%';
         }
     };
 
@@ -59,26 +72,44 @@ document.addEventListener("DOMContentLoaded", (event) => {
         modelWeight = Math.floor(Math.random() * (fontMaxWeight - fontMinWeight + 1)) + fontMinWeight;
         modelWidth = Math.floor(Math.random() * (fontMaxWidth - fontMinWidth + 1)) + fontMinWidth;
 
+        let wordSelection = wordList[Math.floor(Math.random()* wordList.length)];
+
         console.log(model);
         model.style.fontVariationSettings = `"wght" ${modelWeight}, "wdth" ${modelWidth}`;
+        model.textContent = wordSelection;
+        hamburger.textContent = wordSelection;
     }
 
     // Fonction de gestion des "pages"
     function pageGenerator (){
-        if (homeSection === true){
-            homeContainer.style.display="flex";
-            gameContainer.style.display="none";
-        }
-        else{
-            homeContainer.style.display="none";
-            gameContainer.style.display="block";
-        }
+        switch (currentGameState){
+            case gameState.home:
 
-        if (endgame === false){
-            endgamePopUp.style.display="none";
-        }
-        else{
-            endgamePopUp.style.display="flex";
+                // Affichage de la page home
+                homeContainer.style.display="flex";
+                
+                gameContainer.style.display="none";
+                endgamePopUp.style.display="none"
+
+                console.log("currentGameState = " + currentGameState)
+                break;
+                
+            case gameState.game:
+
+                // Affichage de la page home
+                gameContainer.style.display="block";
+                
+                homeContainer.style.display="none";
+                endgamePopUp.style.display="none"
+                console.log("currentGameState = " + currentGameState)
+                break;
+
+            case gameState.endGame:
+
+                // Affichage de la page home
+                endgamePopUp.style.display="flex"
+                console.log("currentGameState = " + currentGameState)
+                break;
         }
     }
 
@@ -101,7 +132,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 isNotFontMatch();
             }
         }
-        
+        else{
+            currentGameState = gameState.endGame;
+        }
     }
 
     // Partie 2  : Calcul des variations des fonts en fonction de la souris
@@ -141,23 +174,121 @@ document.addEventListener("DOMContentLoaded", (event) => {
             record = score;
         }
     }
+    
+    // Fonction de gestion de l'état du jeu
+    function handleGameState (){
+        switch (currentGameState){
+            case gameState.home:
 
-    pageGenerator();
-    modelGenerator();
+                // Instructions de la page home
+                pageGenerator();
+                gameOn = true;
 
-    startButton.addEventListener("click", function(){
-        homeSection = false;
-        pageGenerator();
+                document.addEventListener("mousemove", function (e) {
+                    handleMouseMouvement(e, fontTarget);
+                });
+
+                break;
+            
+            case gameState.game:
+
+                // Instructions de la page jeu
+                pageGenerator();
+                modelGenerator();
+                updateCountdown();
+            
+                
+
+                document.getElementById("score").textContent = `SCORE : ${score}`;
+                document.addEventListener("mousemove", function (e) {
+                    handleMouseMouvement(e, fontTarget);
+                });
+
+
+
+                break;
+            
+            case gameState.endGame:
+
+                // Instructions de la pop up endgame
+
+                recordVerification();
+                pageGenerator();
+
+                document.getElementById("previous-score").textContent = `SCORE : ${score}`;
+                document.getElementById("score").textContent = `RECORD : ${record}`;
+
+                break;
+        }
+    }
+    function handleGameState (){
+        switch (currentGameState){
+            case gameState.home:
+
+                // Instructions de la page home
+                pageGenerator();
+
+                document.addEventListener("mousemove", function (e) {
+                    handleMouseMouvement(e, fontTarget);
+                });
+
+                break;
+            
+            case gameState.game:
+
+                // Instructions de la page jeu
+                pageGenerator();
+                modelGenerator();
+                updateCountdown();
+            
+                
+
+                document.getElementById("score").textContent = `SCORE : ${score}`;
+                document.addEventListener("mousemove", function (e) {
+                    handleMouseMouvement(e, fontTarget);
+                });
+
+
+
+                break;
+            
+            case gameState.endGame:
+
+                // Instructions de la pop up endgame
+
+                recordVerification();
+                pageGenerator();
+                clearInterval();
+
+                document.getElementById("previous-score").textContent = `SCORE : ${score}`;
+                document.getElementById("score").textContent = `RECORD : ${record}`;
+
+                break;
+        }
+    }
+
+    startButton.addEventListener("click", function () {
+        currentGameState = gameState.game;
         fontTarget = hamburger;
+        handleGameState();
     });
 
-    document.getElementById("score").textContent=`SCORE : ${score}`;
-    document.addEventListener("mousemove", function (e) {
-        handleMouseMouvement(e, fontTarget);
+    restartButton.addEventListener("click", function () {
+        currentGameState = gameState.game;
+        fontTarget = hamburger;
+        handleGameState();
+        timeRemaining = countdown
+        console.log("tu as cliqué sur le restart")
     });
 
-    updateCountdown();
-    activeTime = setInterval(updateCountdown, 100);
-
-  
-});
+    homePageButton.addEventListener("click", function () {
+        currentGameState = gameState.home;
+        fontTarget = demoHamburger;
+        handleGameState();
+        console.log("lala");
+        console.log(fontTarget);
+    });
+        
+    // Appel initial de handleGameState
+    handleGameState();
+});  // Fermeture de la fonction globale
